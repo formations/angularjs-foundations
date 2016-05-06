a2bb4c0ae1a3a7729cfe844d481a5abcd3f63225
Nicolas Frankel <nicolas@frankel.ch>
2016-05-07 00:30:39 +0200

    Split single HTML page into index and include

--- Gruntfile.js
+++ Gruntfile.js
@@ -167,7 +167,7 @@ module.exports = function (grunt) {
           base:{
                path: 'dist',
             options: {
-                index: 'store.html',
+                index: 'index.html',
                 maxAge: 300000
             }
           }
